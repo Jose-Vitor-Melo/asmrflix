442de440abf239edee7196528f8f29f2816268e9
Jose Vitor Gonçalves de Melo <64286814+Jose-Vitor-Melo@users.noreply.github.com>
2020-07-30 14:13:12 -0400

    dia 2

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import CadastroVideo from './pages/cadastro/Video'
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import CadastroVideo from './pages/cadastro/Video';
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route path='/' component={App} />
+      <Route path='/' component={App} exact />
+      <Route path='/cadastro/video' component={CadastroVideo} exact />
+      <Route component={() => (<div>Página 404</div>)} />
     </Switch>
   </BrowserRouter>,
   document.getElementById('root')
---
+++ src/pages/cadastro/Video/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Menu from './components/Menu'
+import Footer from './components/Footer'
+
+function CadastroVideo() {
+  return (
+    <div style={{background:"#141414"}}>
+      <Menu />
+      
+      <Footer></Footer>
+    </div>
+  )
+}
+
+export default CadastroVideo;
